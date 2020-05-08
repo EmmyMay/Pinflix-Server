1423a924aa642d130ff50dc2f77bc859cef1de6f
EmmyMay <lekeemmy@gmail.com>
2020-05-08 20:20:56 +0100

    test route

--- router/index.js
+++ router/index.js
@@ -44,6 +44,10 @@ r.get('/pin/tiktok', async (ctx) => {
     }
 })
 
+
+r.get('/', (ctx) => {
+    ctx.body = "Welcome"
+})
 // *******************************************************************
 
 // route to handle Twitter pin posts
